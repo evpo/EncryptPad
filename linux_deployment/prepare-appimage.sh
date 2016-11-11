36881e2050b3e36bed7ffe33578bb2c174bdb88c
Evgeny Pokhilko <software@evpo.net>
2016-11-11 23:12:32 +1100

    qt deployment files and scripts

---
+++ linux_deployment/prepare-appimage.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+USAGE="prepare-appimage.sh <work-directory>"
+
+# work-directory
+# |
+# |-AppDir
+# |-AppImage
+
+if [[ $# < 1 ]]
+then
+    echo $USAGE
+    exit -1
+fi
+
+if ! which AppImageAssistant
+then
+    echo "Cannot find AppImageAssistant"
+    exit -1
+fi
+
+if ! which linuxdeployqt
+then
+    echo "Cannot find linuxdeployqt"
+    exit -1
+fi
+
+if ! which appimagetool
+then
+    echo "Cannot find appimagetool"
+    exit -1
+fi
+
+WORKDIR=$1
+mkdir -p $WORKDIR
+rm -R $WORKDIR/AppDir || true
+rm -R $WORKDIR/encryptpad.AppImage || true
+cp -R ../linux_deployment/AppDir $WORKDIR/
+cp ../bin/release/encryptpad $WORKDIR/AppDir/
+mkdir -p $WORKDIR/AppDir/plugins/platformthemes
+QT_INSTALL_PLUGINS=$(qmake -query | grep "^QT_INSTALL_PLUGINS:" | sed s/^QT_INSTALL_PLUGINS://g)
+cp $QT_INSTALL_PLUGINS/platformthemes/libqgtk2.so $WORKDIR/AppDir/plugins/platformthemes/
+
+cd $WORKDIR/
+linuxdeployqt ./AppDir/encryptpad -bundle-non-qt-libs
+appimagetool ./AppDir encryptpad.AppImage
