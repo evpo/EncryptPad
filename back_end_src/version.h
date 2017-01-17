b44d37ec1d8681c751871f266088a0fea754e533
Evgeny Pokhilko <software@evpo.net>
2017-01-17 18:57:52 +1100

    Bump up the version

--- back_end_src/version.h
+++ back_end_src/version.h
@@ -1,6 +1,6 @@
 #pragma once
-#define VER_FILEVERSION             0,3,2,5
-#define VER_FILEVERSION_STR         "0.3.2.5"
+#define VER_FILEVERSION             0,3,2,6
+#define VER_FILEVERSION_STR         "0.3.2.6"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
