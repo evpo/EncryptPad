b8db89b919797c2e0023e1a9baac18dd5769cfbd
Evgeny Pokhilko <software@evpo.net>
2018-01-28 08:30:21 +1100

    Fix prepare-appimage to support the new linuxdeployqt tool

--- linux_deployment/prepare-appimage.sh
+++ linux_deployment/prepare-appimage.sh
@@ -14,9 +14,9 @@ then
     exit -1
 fi
 
-if ! which AppImageAssistant > /dev/null
+if ! which appimagetool > /dev/null
 then
-    echo "Cannot find AppImageAssistant"
+    echo "Cannot find appimagetool"
     exit -1
 fi
 
@@ -26,22 +26,16 @@ then
     exit -1
 fi
 
-if ! which AppImageAssistant > /dev/null
-then
-    echo "Cannot find AppImageAssistant"
-    exit -1
-fi
-
 WORKDIR=$1
 mkdir -p $WORKDIR
 rm -R $WORKDIR/AppDir || true
 rm -R $WORKDIR/encryptpad.AppImage || true
 cp -R ../linux_deployment/AppDir $WORKDIR/
 cp ../bin/release/encryptpad $WORKDIR/AppDir/
-mkdir -p $WORKDIR/AppDir/plugins/platformthemes
-QT_INSTALL_PLUGINS=$(qmake -query | grep "^QT_INSTALL_PLUGINS:" | sed s/^QT_INSTALL_PLUGINS://g)
-cp $QT_INSTALL_PLUGINS/platformthemes/libqgtk2.so $WORKDIR/AppDir/plugins/platformthemes/
+#mkdir -p $WORKDIR/AppDir/plugins/platformthemes
+#QT_INSTALL_PLUGINS=$(qmake -query | grep "^QT_INSTALL_PLUGINS:" | sed s/^QT_INSTALL_PLUGINS://g)
+#cp $QT_INSTALL_PLUGINS/platformthemes/libqgtk2.so $WORKDIR/AppDir/plugins/platformthemes/
 
 cd $WORKDIR/
 linuxdeployqt ./AppDir/encryptpad -bundle-non-qt-libs
-AppImageAssistant ./AppDir encryptpad.AppImage
+appimagetool ./AppDir encryptpad.AppImage
