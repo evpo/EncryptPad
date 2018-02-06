c998b8717c41e42df537b94cbb6cb3bd6a1a1002
Evgeny Pokhilko <software@evpo.net>
2018-02-06 22:37:56 +1100

    Bumped up the version to 0.4.0.2

--- back_end_src/version.h
+++ back_end_src/version.h
@@ -1,6 +1,6 @@
 #pragma once
-#define VER_FILEVERSION             0,4,0,1
-#define VER_FILEVERSION_STR         "0.4.0.1"
+#define VER_FILEVERSION             0,4,0,2
+#define VER_FILEVERSION_STR         "0.4.0.2"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
