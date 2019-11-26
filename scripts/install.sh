92b28c3da7de6ef6ac8e86885b0bb807bd9e5464
Evgeny Pokhilko <software@evpo.net>
2019-11-26 22:17:43 +1100

    Install the binaries

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! [[ -r "bin/release/encryptpad" ]] || ! [[ -r "bin/release/encryptcli" ]]; then
+    echo "the release binary is not found" >&2
+    exit -1
+fi
+install --mode=755 --owner=0 --group=0 -T bin/release/encryptpad /usr/bin/encryptpad
+install --mode=755 --owner=0 --group=0 -T bin/release/encryptcli /usr/bin/encryptcli
 pushd linux_deployment/de_install >/dev/null
 for f in $(find -type d)
 do
