b22d83cafa8e68e003dcdaca3f1cea60678f039d
Evgeny Pokhilko <software@evpo.net>
2020-04-12 13:21:46 +1000

    remove deprecated signalmapper in favour of a lambda

--- qt_ui/recent_files_service.cpp
+++ qt_ui/recent_files_service.cpp
@@ -20,12 +20,136 @@
 #include "recent_files_service.h"
 
 RecentFilesService::RecentFilesService(QWidget *parent)
-    :signal_mapper_(parent), parent_(parent), menu_(nullptr), begin_index_(0), end_(nullptr), max_files_(0), file_count_(0)
+    :parent_(parent), menu_(nullptr), begin_index_(0), end_(nullptr), max_files_(0), file_count_(0)
 {
-        connect(&signal_mapper_, SIGNAL(mapped(const QString &)), this, SLOT(SignalMapped(const QString &)));
 }
 
-void RecentFilesService::SignalMapped(const QString &str)
+RecentFilesService::~RecentFilesService()
 {
-    emit FileSelected(str);
+}
+
+void RecentFilesService::Init(QMenu *menu, QAction *begin, QAction *end)
+{
+    menu_ = menu;
+    begin_index_ = menu->actions().indexOf(begin);
+    end_ = end;
+}
+
+void RecentFilesService::Serialize(QStringList &list)
+{
+    for(int i = begin_index_ + 1; i < begin_index_ + 1 + file_count_; i++)
+    {
+        QAction *action = menu_->actions().at(i);
+        list.append(action->text());
+        list.append(action->data().toString());
+    }
+}
+
+void RecentFilesService::Deserialize(const QStringList &list, int max_files)
+{
+    max_files_ = max_files;
+    auto it = list.constBegin();
+    while(it != list.constEnd() && file_count_ < max_files)
+    {
+        QString name = *it;
+        it ++;
+        QString path = *it;
+        menu_->insertAction(end_, CreateAction(name, path));
+        file_count_++;
+        it ++;
+    }
+
+    if(file_count_ > 0)
+        menu_->insertSeparator(end_);
+}
+
+void RecentFilesService::PushFile(const QString &file_path)
+{
+    if(max_files_ == 0)
+        return;
+
+    QFileInfo info(file_path);
+    QString name = info.fileName();
+    QString path = info.absoluteFilePath();
+
+    QAction *a = nullptr;
+
+    // remove last separator if exists
+    if(file_count_ > 0)
+    {
+        a = menu_->actions().at(menu_->actions().indexOf(end_) - 1);
+        RemoveAction(a);
+    }
+
+    int i = begin_index_ + 1; // first recent file if exists
+    a = menu_->actions().at(i);
+    menu_->insertAction(a, CreateAction(name, path));
+    file_count_ ++;
+    while(a != end_ && a->data().toString() != path)
+    {
+        i ++;
+        a = menu_->actions().at(i);
+    }
+
+    if(a != end_)
+    {
+        RemoveAction(a);
+        file_count_ --;
+    }
+
+    menu_->insertSeparator(end_);
+
+    SetMaxFiles(max_files_);
+}
+
+void RecentFilesService::SetMaxFiles(int max_files)
+{
+    max_files_ = max_files;
+    if(file_count_ <= max_files_)
+        return;
+
+    int index2remove = menu_->actions().indexOf(end_) - 1;
+    // remove last separator
+    QAction *a = menu_->actions().at(index2remove);
+    RemoveAction(a);
+    index2remove --; // move index to the last recent file
+
+    // Remove recent files from bottom to top
+    while(file_count_ != max_files_)
+    {
+        QAction *a = menu_->actions().at(index2remove);
+        RemoveAction(a);
+        file_count_ --;
+        index2remove --;
+    }
+
+    // return the separator
+    if(max_files_ != 0)
+        menu_->insertSeparator(end_);
+}
+
+int RecentFilesService::GetMaxFiles() const
+{
+    return max_files_;
+}
+
+void RecentFilesService::TriggerFileSelected(QString path)
+{
+    emit FileSelected(path);
+}
+
+QAction *RecentFilesService::CreateAction(const QString &name, const QString &path)
+{
+    QAction *new_action = new QAction(name, parent_);
+    new_action->setData(QVariant(path));
+    new_action->setToolTip(path);
+    new_action->setStatusTip(path);
+    connect(new_action, &QAction::triggered, [this,path](){TriggerFileSelected(path);});
+    return new_action;
+}
+
+void RecentFilesService::RemoveAction(QAction *action)
+{
+    menu_->removeAction(action);
+    action->deleteLater();
 }
--- qt_ui/recent_files_service.h
+++ qt_ui/recent_files_service.h
@@ -35,7 +35,6 @@ class RecentFilesService
 {
     Q_OBJECT
 private:
-    QSignalMapper signal_mapper_;
     QWidget *parent_;
     QMenu *menu_;
     int begin_index_;
@@ -43,138 +42,22 @@ private:
     int max_files_;
     int file_count_;
 
-    QAction *CreateAction(const QString &name, const QString &path)
-    {
-        QAction *new_action = new QAction(name, parent_);
-        new_action->setData(QVariant(path));
-        new_action->setToolTip(path);
-        new_action->setStatusTip(path);
-        signal_mapper_.setMapping(new_action, path);
-        this->connect(new_action, SIGNAL(triggered()), &signal_mapper_, SLOT(map()));
-
-        return new_action;
-    }
-
-    void RemoveAction(QAction *action)
-    {
-        menu_->removeAction(action);
-        signal_mapper_.removeMappings(action);
-        action->deleteLater();
-    }
+    QAction *CreateAction(const QString &name, const QString &path);
+    void RemoveAction(QAction *action);
 signals:
     void FileSelected(const QString &path);
-public slots:
-    void SignalMapped(const QString &str);
 public:
     RecentFilesService(QWidget *parent);
-    virtual ~RecentFilesService(){}
+    virtual ~RecentFilesService();
 
     // begin - separator before recent files, end - next menu item after recent files separator
-    void Init(QMenu *menu, QAction *begin, QAction *end)
-    {
-        menu_ = menu;
-        begin_index_ = menu->actions().indexOf(begin);
-        end_ = end;
-    }
-
-    void Serialize(QStringList &list)
-    {
-        for(int i = begin_index_ + 1; i < begin_index_ + 1 + file_count_; i++)
-        {
-            QAction *action = menu_->actions().at(i);
-            list.append(action->text());
-            list.append(action->data().toString());
-        }
-    }
-
-
-    void Deserialize(const QStringList &list, int max_files)
-    {
-        max_files_ = max_files;
-        auto it = list.constBegin();
-        while(it != list.constEnd() && file_count_ < max_files)
-        {
-            QString name = *it;
-            it ++;
-            QString path = *it;
-            menu_->insertAction(end_, CreateAction(name, path));
-            file_count_++;
-            it ++;
-        }
-
-        if(file_count_ > 0)
-            menu_->insertSeparator(end_);
-    }
-
-    void PushFile(const QString &file_path)
-    {
-        if(max_files_ == 0)
-            return;
-
-        QFileInfo info(file_path);
-        QString name = info.fileName();
-        QString path = info.absoluteFilePath();
-
-        QAction *a = nullptr;
-
-        // remove last separator if exists
-        if(file_count_ > 0)
-        {
-            a = menu_->actions().at(menu_->actions().indexOf(end_) - 1);
-            RemoveAction(a);
-        }
-
-        int i = begin_index_ + 1; // first recent file if exists
-        a = menu_->actions().at(i);
-        menu_->insertAction(a, CreateAction(name, path));
-        file_count_ ++;
-        while(a != end_ && a->data().toString() != path)
-        {
-            i ++;
-            a = menu_->actions().at(i);
-        }
-
-        if(a != end_)
-        {
-            RemoveAction(a);
-            file_count_ --;
-        }
-
-        menu_->insertSeparator(end_);
-
-        SetMaxFiles(max_files_);
-    }
-
-    void SetMaxFiles(int max_files)
-    {
-        max_files_ = max_files;
-        if(file_count_ <= max_files_)
-            return;
-
-        int index2remove = menu_->actions().indexOf(end_) - 1;
-        // remove last separator
-        QAction *a = menu_->actions().at(index2remove);
-        RemoveAction(a);
-        index2remove --; // move index to the last recent file
-
-        // Remove recent files from bottom to top
-        while(file_count_ != max_files_)
-        {
-            QAction *a = menu_->actions().at(index2remove);
-            RemoveAction(a);
-            file_count_ --;
-            index2remove --;
-        }
-
-        // return the separator
-        if(max_files_ != 0)
-            menu_->insertSeparator(end_);
-    }
-
-    int GetMaxFiles() const
-    {
-        return max_files_;
-    }
+    void Init(QMenu *menu, QAction *begin, QAction *end);
+    void Serialize(QStringList &list);
+    void Deserialize(const QStringList &list, int max_files);
+    void PushFile(const QString &file_path);
+    void SetMaxFiles(int max_files);
+    int GetMaxFiles() const;
+    void TriggerFileSelected(QString path);
 };
 
 #endif // RECENT_FILES_SERVICE_H
