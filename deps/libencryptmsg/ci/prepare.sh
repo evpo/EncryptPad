c88f17ea0c7b2952780d2affdaa6acc985cbc21f
Evgeny Pokhilko <software@evpo.net>
2020-05-05 08:44:27 +1000

    Update build scripts from the upstream

--- deps/libencryptmsg/ci/prepare.sh
+++ deps/libencryptmsg/ci/prepare.sh
@@ -7,7 +7,7 @@ git clone https://github.com/randombit/botan $HOME/botan
 ( cd $HOME/botan && git checkout 2.13.0 )
 pushd $HOME/botan
     if [[ "$env_name" == "appveyor" ]]; then
-        ./configure.py --enable-modules=aes,pbkdf2,auto_rng,compression,zlib --amalgamation --disable-shared --cc gcc --cpu x86 --os mingw
+        ./configure.py --cc gcc --cpu x86 --os mingw --amalgamation --disable-shared --with-zlib --with-bzip2 --enable-modules aes,pbkdf2,auto_rng,compression --extra-cxxflags "-I c:\Users\appveyor\zlib-1.2.11 -I c:\Users\appveyor\bzip2-1.0.8"
     elif [[ "$env_name" == "osx" ]]; then
         ./configure.py --enable-modules=aes,pbkdf2,auto_rng,compression,zlib --link-method=symlink --amalgamation --disable-shared --cc-bin clang++
     elif [[ "$env_name" == "linux" ]]; then
