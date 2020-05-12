16137430cdbe8e882f2a2d9ca565e6898dce903a
Evgeny Pokhilko <software@evpo.net>
2020-05-12 21:07:46 +1000

    change the relative directory to fix the bash issue

--- linux_deployment/prepare-appimage.sh
+++ linux_deployment/prepare-appimage.sh
@@ -30,8 +30,8 @@ WORKDIR=$1
 mkdir -p $WORKDIR
 rm -R $WORKDIR/AppDir || true
 rm -R $WORKDIR/encryptpad.AppImage || true
-cp -R ../linux_deployment/AppDir $WORKDIR/
-cp ../bin/release/encryptpad $WORKDIR/AppDir/
+cp -R ./linux_deployment/AppDir $WORKDIR/
+cp ./bin/release/encryptpad $WORKDIR/AppDir/
 #mkdir -p $WORKDIR/AppDir/plugins/platformthemes
 #QT_INSTALL_PLUGINS=$(qmake -query | grep "^QT_INSTALL_PLUGINS:" | sed s/^QT_INSTALL_PLUGINS://g)
 #cp $QT_INSTALL_PLUGINS/platformthemes/libqgtk2.so $WORKDIR/AppDir/plugins/platformthemes/
