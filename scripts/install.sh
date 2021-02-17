c43293dbacf6ed435eca3644d97a348d4353b25f
evpo <software@evpo.net>
2021-02-17 11:27:11 +0000

    update all caches when installing

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if ! [[ -r "bin/release/encryptpad" ]] || ! [[ -r "bin/release/encryptcli" ]]; then
     echo "the release binary is not found" >&2
@@ -17,11 +18,15 @@ do
 done
 popd >/dev/null
 if which update-icon-caches >/dev/null; then
-    update-icon-caches /usr/share/icons/hicolor
+    echo "running upate-icon-caches ..."
+    update-icon-caches /usr/share/icons/*
 fi
 if which update-mime-database >/dev/null; then
+    echo "running update-mime-database ..."
     update-mime-database /usr/share/mime
 fi
 if which update-desktop-database >/dev/null; then
+    echo "running update-desktop-database .."
     update-desktop-database /usr/share/applications
 fi
+echo "installation finished"
