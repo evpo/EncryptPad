a8db7f1459a11a2d4260cf08eec88bd92e4a2783
Evgeny Pokhilko <software@evpo.net>
2023-11-28 22:12:43 +1100

    update the copyright year

--- src/back_end_src/version.h
+++ src/back_end_src/version.h
@@ -8,7 +8,7 @@
 #define VER_COMPANYNAME_STR         ""
 #define VER_FILEDESCRIPTION_STR     "EncryptPad"
 #define VER_INTERNALNAME_STR        "EncryptPad"
-#define VER_LEGALCOPYRIGHT_STR      "Evgeny Pokhilko 2022 Copyright"
+#define VER_LEGALCOPYRIGHT_STR      "Evgeny Pokhilko 2023 Copyright"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "EncryptPad.exe"
